f2349ce290f7df50dfdf8f0b76afc8a7b88709cf
Izzy Edson <isabelle.edson@hyatt.com>
2020-01-03 22:07:05 -0600

    created database and configuration connections

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sweet Potato Black Bean Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Chick-N Burger', true);
